e843374b5cda2ca7dd8eced2d29c6a8778ed9080
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2021-01-30 20:01:39 -0500

    create routers directory and article route file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const articleRouter = require("./routers/article");
 
 const app = express();
-
 const port = 8000;
+
+app.use(articleRouter);
 
 app.listen(port, () => {
   console.log(`Server up on port ${port}`);
