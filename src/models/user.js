e2e9b4ac4fea718069d74f6a4184bad606d8b4d0
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2021-02-04 13:28:59 -0500

    create User model

---
+++ src/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
