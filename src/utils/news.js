57221c394f81a9c89060c04bd9656f17c4921c8d
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2021-02-14 19:39:08 -0500

    refactor logic to return all users dashboard news

--- src/utils/news.js
+++ src/utils/news.js
@@ -1,28 +1,28 @@
-const { createQuery } = require("./queries");
-const newsapi = require("../newsapi");
+/** @format */
+
+const { createQuery } = require('./queries')
+const newsapi = require('../newsapi')
 
 async function getNewsByType(query, q, type) {
   try {
-    const updatedQuery = createQuery(query, { [type]: q });
-    const data = await newsapi.v2.topHeadlines(updatedQuery);
-    const { articles } = data;
-    console.log(data)
-    return { [q]: articles };
+    const updatedQuery = createQuery(query, { [type]: q })
+    const data = await newsapi.v2.topHeadlines(updatedQuery)
+    const { articles } = data
+    return { [q]: articles }
   } catch (error) {
-      console.log(error);
-      return error;
+    console.error(error)
+    return error
   }
-  
 }
 
-
 async function buildUserNews(query, queries, type) {
-  const news = queries.map(q => getNewsByType(query, q, type));
-  const data = await Promise.all(news);
-  const [ catObj1, catObj2 ] = data;
-  return { ...catObj1, ...catObj2 };
+  const news = queries.map(q => getNewsByType(query, q, type))
+  const data = await Promise.all(news)
+  let userNews = {}
+  data.forEach(dataObj => (userNews = { ...userNews, ...dataObj }))
+  return userNews
 }
 
 module.exports = {
   buildUserNews
-};
+}
