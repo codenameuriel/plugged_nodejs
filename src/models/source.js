33547315b743c42b47e9aba4665329b81fa0f8d5
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2021-03-15 19:39:14 -0400

    create Source model

---
+++ src/models/source.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const sourceSchema = new mongoose.Schema({
+  id: {
+    type: String
+  },
+  name: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  url: {
+    type: String
+  },
+  category: {
+    type: String
+  },
+  language: {
+    type: String
+  },
+  country: {
+    type: String
+  },
+  image: {
+    type: String
+  }
+});
+
+const Source = mongoose.model('Source', sourceSchema);
+
+module.exports = Source;
